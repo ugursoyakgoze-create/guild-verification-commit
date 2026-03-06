84389fcdd204ca7cfc528a039bdd8ecbc701bf3b
ugursoyakgoze-create <ugursoyakgoze@gmail.com>
2026-03-06 15:33:44 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:33:45.474Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T12:33:48.529Z
+Commit: Resolve timezone issues in date handling
