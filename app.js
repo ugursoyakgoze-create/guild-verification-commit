c7e1a89355d053fd979df921b4e5da7ef09106e0
ugursoyakgoze-create <ugursoyakgoze@gmail.com>
2026-03-06 15:35:06 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:35:03.698Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T12:35:10.047Z
+Commit: Fix CSS styling bug on mobile devices
