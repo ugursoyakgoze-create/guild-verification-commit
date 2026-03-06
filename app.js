b2369aee355fabdbc3a47a2623843bc9568ee908
ugursoyakgoze-create <ugursoyakgoze@gmail.com>
2026-03-06 15:35:39 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:35:34.374Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-06T12:35:43.322Z
+Commit: Resolve null pointer exception in user service
