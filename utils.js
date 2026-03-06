94b443804ca55ef30c97bfab0e1b6bd04524280f
ugursoyakgoze-create <ugursoyakgoze@gmail.com>
2026-03-06 15:35:38 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:35:41.480Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T12:35:42.465Z
+Commit: Fix CSS styling bug on mobile devices
