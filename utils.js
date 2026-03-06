802bc9f244b7f3a78d7c948bc9439d7802151181
ugursoyakgoze-create <ugursoyakgoze@gmail.com>
2026-03-06 15:35:42 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:35:42.465Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T12:35:45.868Z
+Commit: Add export functionality for reports
