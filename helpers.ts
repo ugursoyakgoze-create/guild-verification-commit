b0410454248045b730562b4f069f58193f03a654
ugursoyakgoze-create <ugursoyakgoze@gmail.com>
2026-03-06 15:35:05 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:35:02.794Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-06T12:35:09.191Z
+Commit: Resolve null pointer exception in user service
