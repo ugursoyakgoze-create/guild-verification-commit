2b4dd3e37d5c10443e10fd171fa10d0e94963115
ugursoyakgoze-create <ugursoyakgoze@gmail.com>
2026-03-06 15:35:36 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:35:25.021Z
-Commit: Add multi-language support
+Timestamp: 2026-03-06T12:35:39.712Z
+Commit: Resolve null pointer exception in user service
