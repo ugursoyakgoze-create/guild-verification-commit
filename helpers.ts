32ae2d5d3dacdbd65c28625385986be608758dcc
ugursoyakgoze-create <ugursoyakgoze@gmail.com>
2026-03-06 15:34:50 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:34:53.639Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T12:34:54.444Z
+Commit: Fix memory leak in event handlers
